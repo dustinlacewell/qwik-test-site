c04772dc10ec420f97b03ec187059b28aa54f97a
Dustin Lacewell <dlacewell@gmail.com>
2024-08-03 10:16:19 -0500

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,10 @@
+import qwik from "@qwikdev/astro";
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  integrations: [qwik()],
+});
