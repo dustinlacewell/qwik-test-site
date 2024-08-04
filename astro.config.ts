3fb41e38400e2d6b57b37f707236ba1e7ae042dc
Dustin Lacewell <dlacewell@gmail.com>
2024-08-04 03:44:31 -0500

    Add base url

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
+import react from "@astrojs/react";
 import qwik from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [qwik()],
+  base: "/demo-template/",
+  integrations: [qwik({ include: "src/components/*" }), react()],
 });
